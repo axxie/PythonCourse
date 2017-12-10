cfd61185e5b96259ef1aff1e04ecc5b15483565f
Axxie <none@none>
2017-12-10 17:47:15 +0200

    Add support for producing program output separately from the code

--- ex1.1.rst
+++ ex1.1.rst
@@ -2,8 +2,13 @@ Example 1.1 - first steps
 =========================
 
 .. code_example:: pycon
+:name: prints
 
-    print("Hello World!")
-    print("Hello Again")
-    print("I like typing this.")
+    print("Hello, World!")
+    print("Another line?")
+    print("More lines.")
 
+
+Run the above example and you should see this:
+
+.. code_output:: prints
--- ext/code_example/__init__.py
+++ ext/code_example/__init__.py
@@ -7,12 +7,13 @@ Run the code and insert stdout after the code block.
 """
 import hashlib
 import os
-from subprocess import PIPE, Popen
+import os.path
+import tempfile
+from subprocess import PIPE, Popen, DEVNULL
 
 from docutils import nodes
 from docutils.parsers.rst import Directive, directives
 from sphinx.errors import SphinxError
-
 from sphinx_autorun import version
 
 __version__ = version.version
@@ -40,6 +41,33 @@ class AutoRun(object):
         cls.config.update(app.builder.config.code_example_languages)
 
 
+class codeout_node(nodes.General, nodes.Element):
+    pass
+
+
+class CodeOutput(Directive):
+    has_content = False
+    required_arguments = 1
+    optional_arguments = 0
+    final_argument_whitespace = False
+
+    def run(self):
+        code_out = codeout_node()
+        code_out['ref'] = self.arguments[0]
+        return [code_out]
+
+
+def render_source(source, image_path):
+    new_file, filename = tempfile.mkstemp(suffix=".py")
+    os.write(new_file, source)
+    os.close(new_file)
+
+    process = Popen(["cmd.exe", "/c", os.path.join(os.path.dirname(__file__), "convert.cmd"), filename, image_path],
+                    stdout=DEVNULL, stderr=DEVNULL)
+    process.wait()
+    os.remove(filename)
+
+
 class RunBlock(Directive):
     has_content = True
     required_arguments = 1
@@ -47,6 +75,7 @@ class RunBlock(Directive):
     final_argument_whitespace = False
     option_spec = {
         'linenos': directives.flag,
+        'name': directives.unchanged
     }
 
     def run(self):
@@ -59,54 +88,71 @@ class RunBlock(Directive):
             raise RunBlockError('Unknown language %s' % language)
 
         # Get configuration values for the language
-        args = config[language].split()
         input_encoding = config.get(language + '_input_encoding', 'ascii')
-        output_encoding = config.get(language + '_output_encoding', 'ascii')
-        prefix_chars = config.get(language + '_prefix_chars', 0)
-        show_source = config.get(language + '_show_source', True)
 
-        codelines = (line[prefix_chars:] for line in self.content)
+        codelines = self.content[:]
         code = u'\n'.join(codelines).encode(input_encoding)
 
         h = hashlib.sha1()
         h.update(code)
         key = h.hexdigest()
 
-        file = os.path.join(statics_dir_path, "code_example_%s.png" % (key))
-        args += [file]
+        image_path = os.path.join(statics_dir_path, "code_example_%s.png" % key)
+        render_source(code, image_path)
 
-        # Build the code text
-        proc = Popen(args, bufsize=1, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        image = nodes.image(uri=image_path)
 
-        # Run the code
-        stdout, stderr = proc.communicate(code)
+        env = self.state.document.settings.env
+        if not hasattr(env, 'code_outs'):
+            env.code_outs = {}
 
-        # Process output
-        if stdout:
-            out = stdout.decode(output_encoding)
-        if stderr:
-            out = stderr.decode(output_encoding)
+        name = self.options.get('name')
+        if name:
+            env.code_outs[name] = code
 
-        # Get the original code with prefixes
-        if show_source:
-            code = u'\n'.join(self.content)
-        else:
-            code = ''
-        code_out = u'\n'.join((code, out))
+        return [image]
 
-        if code_out[0] == '\n':
-            code_out = code_out[1:]
 
-        image = nodes.image(uri=file)
+def execute_codeblock(code):
+    config = AutoRun.config
+    args = config['pycon'].split()
+    # Build the code text
+    proc = Popen(args, bufsize=1, stdin=PIPE, stdout=PIPE, stderr=PIPE)
 
-        # TODO: add execution result where requested, not immediately after picture
-        literal = nodes.literal_block(code_out, code_out)
-        literal['language'] = language
-        literal['linenos'] = 'linenos' in self.options
-        return [image, literal]
+    # Run the code
+    stdout, stderr = proc.communicate(code)
+
+    # Process output
+    if stdout:
+        out = stdout.decode("utf-8")
+    if stderr:
+        out = stderr.decode("utf-8")
+
+    # Get the original code with prefixes
+    code_out_text = out
+
+    if code_out_text[0] == '\n':
+        code_out_text = code_out_text[1:]
+
+    return code_out_text
+
+
+def process_codeblock_nodes(app, doctree, fromdocname):
+    env = app.builder.env
+
+    for node in doctree.traverse(codeout_node):
+        content = []
+        code_out_text = execute_codeblock(env.code_outs[node['ref']])
+        literal = nodes.literal_block(code_out_text, code_out_text)
+        literal['language'] = "console"
+        literal['linenos'] = False
+        content.append(literal)
+        node.replace_self(content)
 
 
 def setup(app):
     app.add_directive('code_example', RunBlock)
+    app.add_directive('code_output', CodeOutput)
     app.connect('builder-inited', AutoRun.builder_init)
+    app.connect('doctree-resolved', process_codeblock_nodes)
     app.add_config_value('code_example_languages', AutoRun.config, 'env')
