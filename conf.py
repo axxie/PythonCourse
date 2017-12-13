53e73423d5e46b572781cc8e22d7f60b0f20948f
Axxie <none@none>
2017-12-13 09:45:17 +0200

    Disable linking to original images
    By default, sphinx links image to original if it is resized.
    For the course it doesn't make sense, as we use hi-res logos of products.

--- conf.py
+++ conf.py
@@ -98,7 +98,7 @@ html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-
+html_scaled_image_link = False
 
 # -- Options for HTMLHelp output ------------------------------------------
 
