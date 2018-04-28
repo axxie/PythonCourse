6b5fe2d7ae4e9a22ced2c4323936ba8b5f803269
Axxie <none@none>
2018-04-28 07:01:45 +0300

    Publish rendered files

--- ext/code_example/convert.cmd
+++
@@ -1,9 +0,0 @@
-set HOME=%~dp0
-set NAME=%~n1
-set OUT_DIR=%~dp2
-"%HOME%\colorer\colorer.exe" -c "%HOME%\colorer\base\catalog.xml" -h "%1" -dc -i htmlcss >"%TEMP%\%NAME%.html" 2>"%HOME%\convert.log"
-mkdir "%OUT_DIR%"
-copy "%HOME%\hrdstyle.css" "%TEMP%"
-"%HOME%\ieCapt.exe" --min-width=640 --url="file:///%TEMP%\%NAME%.html" --out="%2" >>"%HOME%\convert.log" 2>>&1
-del "%TEMP%\%NAME%.html"
-del "%TEMP%\hrdstyle.css"
